f97ce5cb8f1dc9a36a45e312074c88a27a5f6a1a
Chitransh Gupta <31571545+Calm-Rock@users.noreply.github.com>
2019-02-16 01:14:18 +0530

    Update CNN.py

--- Project_1/CNN.py
+++ Project_1/CNN.py
@@ -1,10 +1,15 @@
+#Code Implementation of a Deep CNN which was used in prediciton of soil organic carbon
+#For each sample, 3 images were taken by teh setup.
+# The hyper parameters can be tweked to get the best result.
+#Required Libraries
 import numpy as np
 import pandas as pd
 import os
 from skimage.transform import resize
 from skimage.io import imread
 
-y=np.asarray(pd.read_csv('/home/somchakr/different_features_values_of_all_samples.csv')['carbon_percent'])
+
+y=np.asarray(pd.read_csv('/home/somchakr/different_features_values_of_all_samples.csv')['carbon_percent']) #Path to csv file containing carbon percent values
 Y=[]
 for i in range(len(y)):
   a=np.full((3,1), y[i])
@@ -12,7 +17,7 @@ for i in range(len(y)):
   
 y=np.concatenate(Y)
 images = []
-for image_path in sorted(os.listdir('/home/somchakr/Final_numbered_images_CNN')):
+for image_path in sorted(os.listdir('/home/somchakr/Final_numbered_images_CNN')): #sorting out the images by number
     path='/home/somchakr/Final_numbered_images_CNN'+ '/' + image_path
     print(path)
     img = imread(path)
@@ -21,9 +26,10 @@ for image_path in sorted(os.listdir('/home/somchakr/Final_numbered_images_CNN'))
 X=np.array(images)    
 print(X[0].shape)
 from sklearn.model_selection import train_test_split
-
+#Splitting the data set as 75% training and 25% validation
 X_train, X_test, y_train, y_test = train_test_split(X, y,  test_size=0.25)
 
+#Required Libraries
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D, Dropout,BatchNormalization
@@ -76,8 +82,10 @@ regressor.add(Dense(units = 1, activation = 'linear'))
 regressor.compile(optimizer = 'adam', loss = 'mse', metrics = ['mae'])
 regressor.fit(X_train, y_train, batch_size =16, epochs =20)
 
+# Predicting the carbon values
 y_pred_test = regressor.predict(X_test)
 Y_test=pd.DataFrame(data=y_test, columns=['Y_TEST'])
 Y_pred_test=pd.DataFrame(data=y_pred_test, columns=['Y_PRED_TEST'])
 Y=pd.concat([Y_test, Y_pred_test], axis =1)
+# Saving the results 
 Y.to_csv('/home/somchakr/results_full.csv') 
